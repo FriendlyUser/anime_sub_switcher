6f274a0281bc89087fffb38d8c76c421556bd57a
David Li <davidli012345@gmail.com>
2023-01-02 05:41:01 +0000

    feat: update anime sub switcher

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ To practise my cantonese, find cantonese dub videos, and replace the subs with e
 To get the file, I used youtube-dl and manually downloaded the video, in the future just extract the mu38 link from the url
 
 For example for ktkkt top.
+
 <iframe width="100%" height="100%" src="dplayer/dplayer.html?videourl=/play/13914-1-260.html,https://v4.szjal.cn/20200816/Pk8C9nha/index.m3u8,,13914,1,260" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>
 
 To extract the audio stream without re-encoding:
@@ -15,3 +16,10 @@ Then combine the old audio with the english sub
 ```bash
 ffmpeg -i v.mp4 -i a.wav -c:v copy -map 0:v:0 -map 1:a:0 new.mp4
 ```
+
+
+Do an animated movie https://www.ktkkt.top/play/15560-0-0.html
+
+demon slayer the infinity train
+
+# https://nyaa.si/view/1382745
--- downloader.py
+++ downloader.py
@@ -1,1 +1,56 @@
 # youtube_dl download https://v4.szjal.cn/20200816/Pk8C9nha/index.m3u8
+# youtube_dl download https://v4.szjal.cn/20200816/Pk8C9nha/1000k/hls/index.m3u8
+
+import os
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
+
+
+def download(url):
+    cmd = 'youtube_dl ' + url
+    os.system(cmd)
+
+# extracts m3u8 link from the video url
+# 
+def extract_url(video_url):
+    if video_url.endswith('.m3u8'):
+        return video_url
+    else:
+        return video_url + '/index.m3u8'
+
+# read REMOTE_SELENIUM_KEY and REMOTE_SELENIUM_URL from environment variables
+
+REMOTE_SELENIUM_KEY = os.environ.get('REMOTE_SELENIUM_KEY')
+REMOTE_SELENIUM_USERNAME = os.environ.get('REMOTE_SELENIUM_USERNAME')
+
+def make_webdriver(build_name="anime_sub_switcher"):
+    # make a webdriver
+    url = f"https://{REMOTE_SELENIUM_USERNAME}:{REMOTE_SELENIUM_KEY}@hub-cloud.browserstack.com/wd/hub"
+    desired_cap = {
+        "build": build_name,  # CI/CD job or build name
+    }
+    driver = webdriver.Remote(
+        command_executor=url
+    )
+    return driver
+# selenium browser
+
+def get_ktkkt_video_url(ktkkt_url: str):
+    driver = make_webdriver()
+    # 
+    time.sleep(5)
+
+    # wait for the page to load
+    driver.get(ktkkt_url)
+    # switch to cciframe
+    driver.switch_to.frame('cciframe')
+    # wait for the video to load
+    time.sleep(3)
+    # save source to file
+    with open('source.html', 'w') as f:
+        f.write(driver.page_source)
+
+    driver.quit()
